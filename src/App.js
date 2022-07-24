ae546109bb290a6a97bf82a3a62435e4d206e6b6
Senad <senadshabaj73@gmail.com>
2022-07-24 18:04:02 +0200

    Using Stateful Lists

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/Expenses/NewExpense/NewExpense";
-const App = () => {
-  const expenses = [
+
+const DUMMY_EXPENSES = [
     {
       id: "e1",
       title: "Toilet Paper",
@@ -23,9 +24,13 @@ const App = () => {
     },
   ];
 
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expenses);
+    setExpenses((prevExpenses) => {
+      return [expense, ...expenses];
+    });
   };
 
   return (
